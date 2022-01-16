a92b3c612ffa9971934d48537c18308743b393c1
Chetan Panchal <Chetan.Panchal@Achilles.com>
2022-01-16 11:29:13 +0000

    Initial Checkin

---
+++ customcaching/Sample.CustomCaching.Tests/CustomCachingTests.cs
@@ -0,0 +1,8 @@
+using Xunit;
+
+namespace Sample.CustomCaching.Tests
+{
+    public class CustomCachingTests
+    {
+    }
+}
---
+++ customcaching/Sample.CustomCaching/CustomCaching.cs
@@ -0,0 +1,7 @@
+﻿namespace Sample.CustomCaching
+{
+    public class CustomCaching
+    {
+
+    }
+}
